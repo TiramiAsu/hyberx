291e3d4e37073c1b0cfeddd48310cbfed12c4ab2
CBK <cbkcbkcbkcbk@yahoo.com.tw>
2020-06-15 11:37:44 +0800

    ADD [ Account,Member ] 加上JPA註解

--- src/main/java/tw/com/hyberx/model/entity/Account.java
+++ src/main/java/tw/com/hyberx/model/entity/Account.java
@@ -1,22 +1,62 @@
 package tw.com.hyberx.model.entity;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Accounts {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "account")
+public class Account implements Serializable{
+	
+	private static final long serialVersionUID = -5608241289036967005L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "name",length = 255,nullable = false)
 	private String name;
+	
+	@Column(name = "password",nullable = false)
 	private String password;
-	private Members m_id;
+	
+	@ManyToOne
+	@JoinColumn(name = "m_id",nullable = false)
+	private Member m_id;
+	
+	@Column(name = "status",nullable = false)
 	private int status;
+	
+	@Column(name = "error_times",nullable = false)
 	private int error_times;
+	
+	@Column(name = "time_build", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
 	private Date time_build;
+	
+	@Column(name = "time_modify", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
 	private Date time_modify;
-	public Accounts() {
-		super();
-	}
-	public Accounts(String name, String password, Members m_id, int status, int error_times, Date time_build,
+	
+	public Account() {}
+	
+	public Account(String name, 
+			String password,
+			Member m_id,
+			int status,
+			int error_times,
+			Date time_build,
 			Date time_modify) {
-		super();
 		this.name = name;
 		this.password = password;
 		this.m_id = m_id;
@@ -25,54 +65,71 @@ public class Accounts {
 		this.time_build = time_build;
 		this.time_modify = time_modify;
 	}
+	
 	public Long getId() {
 		return id;
 	}
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getPassword() {
 		return password;
 	}
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public Members getM_id() {
+	
+	public Member getM_id() {
 		return m_id;
 	}
-	public void setM_id(Members m_id) {
+	
+	public void setM_id(Member m_id) {
 		this.m_id = m_id;
 	}
+	
 	public int getStatus() {
 		return status;
 	}
+	
 	public void setStatus(int status) {
 		this.status = status;
 	}
+	
 	public int getError_times() {
 		return error_times;
 	}
+	
 	public void setError_times(int error_times) {
 		this.error_times = error_times;
 	}
+	
 	public Date getTime_build() {
 		return time_build;
 	}
+	
 	public void setTime_build(Date time_build) {
 		this.time_build = time_build;
 	}
+	
 	public Date getTime_modify() {
 		return time_modify;
 	}
+	
 	public void setTime_modify(Date time_modify) {
 		this.time_modify = time_modify;
 	}
+	
 	@Override
 	public String toString() {
 		return "Accounts [id=" + id + ", name=" + name + ", password=" + password + ", m_id=" + m_id + ", status="
--- src/main/java/tw/com/hyberx/model/entity/Member.java
+++ src/main/java/tw/com/hyberx/model/entity/Member.java
@@ -1,81 +1,128 @@
 package tw.com.hyberx.model.entity;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Members {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "member")
+public class Member implements Serializable{
+	
+	private static final long serialVersionUID = -5608241289036967005L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "name",length = 255,nullable = false)
 	private String name;
+	
+	@Column(name = "address",nullable = false)
 	private String address;
+	
+	@Column(name = "phone",nullable = false)
 	private String phone;
+	
+	@Column(name = "email",nullable = false)
 	private String email;
-	private String agent;
+	
+	@Column(name = "time_build", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
 	private Date time_build;
+	
+	@Column(name = "time_modify", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
 	private Date time_modify;
-	public Members() {
-		super();
-	}
-	public Members(String name, String address, String phone, String email, String agent, Date time_build,
+	
+	public Member() {}
+	
+	public Member(String name, 
+			String address, 
+			String phone, 
+			String email, 
+			Date time_build,
 			Date time_modify) {
-		super();
 		this.name = name;
 		this.address = address;
 		this.phone = phone;
 		this.email = email;
-		this.agent = agent;
 		this.time_build = time_build;
 		this.time_modify = time_modify;
 	}
+	
 	public Long getId() {
 		return id;
 	}
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getAddress() {
 		return address;
 	}
+	
 	public void setAddress(String address) {
 		this.address = address;
 	}
+	
 	public String getPhone() {
 		return phone;
 	}
+	
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getAgent() {
-		return agent;
-	}
-	public void setAgent(String agent) {
-		this.agent = agent;
-	}
+	
 	public Date getTime_build() {
 		return time_build;
 	}
+	
 	public void setTime_build(Date time_build) {
 		this.time_build = time_build;
 	}
+	
 	public Date getTime_modify() {
 		return time_modify;
 	}
+	
 	public void setTime_modify(Date time_modify) {
 		this.time_modify = time_modify;
 	}
+	
 	@Override
 	public String toString() {
-		return "Members [id=" + id + ", name=" + name + ", address=" + address + ", phone=" + phone + ", email=" + email
-				+ ", agent=" + agent + ", time_build=" + time_build + ", time_modify=" + time_modify + "]";
+		return "Members [id=" + id +
+				", name=" + name +
+				", address=" + address +
+				", phone=" + phone +
+				", email=" + email +
+				", time_build=" + time_build +
+				", time_modify=" + time_modify +
+				"]";
 	}
 }
