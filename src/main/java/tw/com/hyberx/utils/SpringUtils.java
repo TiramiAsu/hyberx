f5ac138b64ed928c30621efbe25d1ab91cbb2171
CBK <cbkcbkcbkcbk@yahoo.com.tw>
2020-06-29 17:13:07 +0800

    CHG [ Account,Member ] pom.xml加jackson SpringUtils把自己加的移除

--- src/main/java/tw/com/hyberx/utils/SpringUtils.java
+++ src/main/java/tw/com/hyberx/utils/SpringUtils.java
@@ -16,12 +16,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
-<<<<<<< HEAD
-import tw.com.hyberx.model.entity.Account;
-import tw.com.hyberx.model.entity.Member;
-=======
 import tw.com.hyberx.model.entity.BasicInfo;
->>>>>>> 5ae6d0bebca89f10a28c0ca4b33649bd428ef84a
+
 import tw.com.hyberx.model.entity.Student;
 import tw.com.hyberx.service.AccountService;
 import tw.com.hyberx.service.MemberService;
@@ -66,24 +62,6 @@ public class SpringUtils {
 		} else {
 		    List<Student> list = studentService.query();
 		    list.add(new Student(new BasicInfo("Asia", 22)));
-		    list.stream().forEach(System.out::println);
-			System.out.println(">>> Success <<<");
-		}
-		MemberService memberService = SpringUtils.getBean(MemberService.class);
-		if (memberService == null) {
-			System.out.println(">>> null <<<");
-		} else {
-		    List<Member> list = memberService.query();
-		    list.add(new Member("a", "a", "1", "a@a.a", new Date(), new Date()));
-		    list.stream().forEach(System.out::println);
-			System.out.println(">>> Success <<<");
-		}
-		AccountService accountService = SpringUtils.getBean(AccountService.class);
-		if (accountService == null) {
-			System.out.println(">>> null <<<");
-		} else {
-		    List<Account> list = accountService.query();
-		    list.add(new Account("a", "a", null, 0, 0, new Date(), new Date()));
 		    list.stream().forEach(System.out::println);
 			System.out.println(">>> Success <<<");
 		}
